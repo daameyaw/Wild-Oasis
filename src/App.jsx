027289df3f57913f8e90c2ca1472279e24ad754a
Lhegacy <davidameyaw607@gmail.com>
2024-06-26 20:39:31 -0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import styled from "styled-components";
 
+const H1 = styled.h1`
+font-size: 30px
+font-weight : 600
+`;
 export default function App() {
   return (
     <div>
-      Hello
+      <H1>Hello World</H1>
     </div>
-  )
+  );
 }
